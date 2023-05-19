c63a92bc1fda57262360f94c8ab61b010043c0ef
SabanadzeLuka <123399167+SabanadzeLuka@users.noreply.github.com>
2023-05-19 19:06:25 +0400

    </>

---
+++ react-todo/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+// importing components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect(() => {
+    filterHander();
+  }, [todos, status]);
+
+  const filterHander = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+    }
+  };
+  return (
+    <div className="App">
+      <header>
+        <h1>Todo List</h1>
+      </header>
+      <Form
+        inputText={inputText}
+        setInputText={setInputText}
+        todos={todos}
+        setTodos={setTodos}
+        setStatus={setStatus}
+      />
+      <TodoList todos={todos} setTodos={setTodos} filteredTodos={filteredTodos} />
+    </div>
+  );
+}
+
+export default App;
